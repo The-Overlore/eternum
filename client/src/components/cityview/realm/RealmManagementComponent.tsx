f469b1072c67b9c7328c9697f86a46ee91516873
Timothy Edison <timothyedison@hotmail.com>
2023-10-23 11:01:46 +0900

    Add generation of message on harvest

--- client/src/components/cityview/realm/RealmManagementComponent.tsx
+++ client/src/components/cityview/realm/RealmManagementComponent.tsx
@@ -14,6 +14,7 @@ import useRealmStore from "../../../hooks/store/useRealmStore";
 import RealmCombatComponent from "./RealmCombatComponent";
 import RealmInfoComponent from "./RealmInfoComponent";
 import { RealmBuildingsComponent } from "./RealmBuildingsComponent";
+import { NpcProvider } from "../../../NpcContext";
 
 const RealmManagementComponent = () => {
   const realmEntityId = useRealmStore((state) => state.realmEntityId);
