bdc6d771a035daa4b8141dbf14e571d7d1d4d9c8
Timothy Edison <timothyedison@hotmail.com>
2023-12-27 16:55:18 +0100

    Use SOZO_WORLD env var in the torii.sh scripts + remove line from package.json + add tty to lore-machine for error in the makefile + fix react-websocket version

--- contracts/scripts/torii.sh
+++ contracts/scripts/torii.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 set -e
 
-set_config() {
-	source ./scripts/env_variables.sh dev
-	export STARKNET_RPC_URL=http://katana:5050
-	printf '0.1\n' | ./scripts/set_config.sh
-}
+source ./scripts/env_variables.sh dev
 
-sozo migrate --rpc-url http://katana:5050
+export STARKNET_RPC_URL=http://katana:5050
 
-# TODO: Get world address from migrate
-torii --world 0x5cb4ce060de62a7b6bfd4cb70fd4ae3196bfe399c372cb0863e632ba8cc73ef --rpc http://katana:5050 &
+sozo migrate --rpc-url $STARKNET_RPC_URL
+
+torii --world $SOZO_WORLD --rpc $STARKNET_RPC_URL &
 
 # echo "Waiting for torii to boot..."
 sleep 5
 
 if [ -z "$RESTART_TORII" ]; then
-	set_config
+	printf '0.1\n' | ./scripts/set_config.sh
 fi
 
 sleep 1000000000000
