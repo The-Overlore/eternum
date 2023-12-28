21f67b7d77d557376afb0152bc38bd76eb29b904
cwastche <wastchenko.constantin@gmail.com>
2023-12-28 13:09:45 +0100

    Npc list panel w/ dummy data

--- client/src/components/cityview/realm/RealmNpcComponent.tsx
+++ client/src/components/cityview/realm/RealmNpcComponent.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { Tabs } from "../../../elements/tab";
 import { NpcPanel } from "./villagers/NpcPanel";
+import { NpcListPanel } from "./villagers/NpcListPanel";
 import useRealmStore from "../../../hooks/store/useRealmStore";
 import useUIStore from "../../../hooks/store/useUIStore";
 import { useRoute, useLocation } from "wouter";
@@ -13,6 +14,7 @@ export const RealmNpcComponent = ({}: RealmVillagersComponentProps) => {
 
   const moveCameraToLaborView = useUIStore((state) => state.moveCameraToLaborView);
   const moveCameraToFoodView = useUIStore((state) => state.moveCameraToFoodView);
+  const setTooltip = useUIStore((state) => state.setTooltip);
 
   // @ts-ignore
   const [location, setLocation] = useLocation();
@@ -21,9 +23,12 @@ export const RealmNpcComponent = ({}: RealmVillagersComponentProps) => {
 
   useEffect(() => {
     let _tab: string = "";
-    if (["farmers"].includes(params?.tab as string)) {
+    // if (["farmers"].includes(params?.tab as string)) {
+    if (["villagers"].includes(params?.tab as string)) {
+      _tab = "villagers";
       moveCameraToFoodView();
     } else {
+      _tab = params?.tab as any;
       moveCameraToLaborView();
     }
     const tabIndex = tabs.findIndex((tab) => tab.key === _tab);
@@ -35,10 +40,24 @@ export const RealmNpcComponent = ({}: RealmVillagersComponentProps) => {
   const tabs = useMemo(
     () => [
       {
-        key: "villagers",
-        label: <div>All</div>,
+        key: "Townhall",
+        label: <div>Townhall</div>,
         component: <NpcPanel />,
       },
+      {
+        key: "villagers",
+        label: (
+          <div 
+            onMouseEnter={() => setTooltip({ position: "bottom", content: <p className="whitespace-nowrap">Tooltip</p> })}
+            onMouseLeave={() => setTooltip(null)}
+            className="flex relative group flex-col items-center"
+          >
+            Villagers
+          </div>
+        ),
+        component: <NpcListPanel />,
+      },
+      
     ],
     [selectedTab],
   );
