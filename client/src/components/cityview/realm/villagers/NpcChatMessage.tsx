f4e67c043bcc399d8d5f883424c7e79a71b94369
Timothy Edison <timothyedison@hotmail.com>
2024-02-23 15:59:39 +0100

    Refactor

--- client/src/components/cityview/realm/villagers/NpcChatMessage.tsx
+++ client/src/components/cityview/realm/villagers/NpcChatMessage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, RefObject } from "react";
+import { useNpcContext } from "./NpcContext";
 
 const INTERKEY_STROKEN_DURATION_MS = 25;
 const CHARACTER_NUMBER_PER_LINE = 64;
@@ -6,39 +7,34 @@ const CHARACTER_NUMBER_PER_LINE = 64;
 export interface NpcChatMessageProps {
   npcName: string;
   dialogueSegment: string;
-  index: number;
-  setLastMessageDisplayedIndex: any;
-  selectedTownhall: number;
+  msgIndex: number;
   viewed: boolean;
   bottomRef: RefObject<HTMLElement>;
 }
 
 export function useTypingEffect(
   msgIndex: number,
-  setLastMessageDisplayedIndex: any,
   bottomRef: RefObject<HTMLElement>,
   textToType: string,
-  selectedTownhall: number,
   interKeyStrokeDurationInMs: number,
 ) {
-  const [displayedText, setDisplayedText] = useState('');
-
+  const [displayedText, setDisplayedText] = useState("");
+  const { selectedTownhall, setLastMessageDisplayedIndex } = useNpcContext();
   useEffect(() => {
-    setDisplayedText('');
+    setDisplayedText("");
 
     const intervalId = setInterval(() => {
       setDisplayedText((prev) => {
         if (prev.length < textToType.length) {
-          
           if (prev.length % CHARACTER_NUMBER_PER_LINE == 0) {
             setTimeout(() => {
               if (bottomRef.current) {
                 bottomRef.current.scrollIntoView({ behavior: "smooth", block: "nearest" });
               }
             }, 1);
-          };
+          }
           return textToType.slice(0, prev.length + 1);
-        } 
+        }
         clearInterval(intervalId);
         setLastMessageDisplayedIndex(msgIndex + 1);
         return prev;
@@ -46,17 +42,18 @@ export function useTypingEffect(
     }, interKeyStrokeDurationInMs);
     return () => clearInterval(intervalId);
   }, [msgIndex, textToType, setLastMessageDisplayedIndex, interKeyStrokeDurationInMs, selectedTownhall]);
-  if (textToType === undefined) return "";
-  return displayedText;
-};
 
+  if (textToType === undefined) return "";
+
+  return displayedText;
+}
 
 export const NpcChatMessage = (props: NpcChatMessageProps) => {
-  const { npcName, dialogueSegment, index, setLastMessageDisplayedIndex, selectedTownhall, bottomRef, viewed } = props;
-  
-  const typedDialogSegment = viewed
+  const { msgIndex, npcName, dialogueSegment, bottomRef, viewed: wasAlreadyViewed } = props;
+
+  const displayedDialogSegment = wasAlreadyViewed
     ? dialogueSegment
-    : useTypingEffect(index, setLastMessageDisplayedIndex, bottomRef, dialogueSegment, selectedTownhall, INTERKEY_STROKEN_DURATION_MS);
+    : useTypingEffect(msgIndex, bottomRef, dialogueSegment, INTERKEY_STROKEN_DURATION_MS);
 
   return (
     <div className="flex flex-col px-2 mb-3 py-1">
@@ -70,7 +67,7 @@ export const NpcChatMessage = (props: NpcChatMessageProps) => {
             </div>
           </div>
           <div style={{ userSelect: "text" }} className="mt-1 text-xs text-white/70">
-            {typedDialogSegment}
+            {displayedDialogSegment}
           </div>
         </div>
       </div>
